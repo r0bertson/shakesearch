1f035a5047619975afbc0cf527d49c14bc54d220
Robertson Lima <robert@wisk.ai>
2022-09-06 16:53:19 -0300

    style: UI improvements

--- static/app.js
+++ static/app.js
@@ -5,18 +5,25 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateContent(results);
       });
     });
   },
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}</tr>`);
-    }
-    table.innerHTML = rows;
+  updateContent: (results) => {
+    const content = document.getElementById("content");
+    const blocks = [];
+
+    results.forEach((result) => {
+      const innerBlocks = [];
+      let fragments = result.fragments
+      let title = result.work_title
+      fragments.forEach((fragment) => {
+        innerBlocks.push(`<div class="card border-dark mb-3"><div class="card-body">${fragment}</div></div>`)
+      })
+      blocks.push(`<h3>${title}</h3>${innerBlocks.join("\n")}`)
+    })
+    content.innerHTML = blocks.join("\n");
   },
 };
 
